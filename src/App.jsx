246d85df03d0c194b9644b36021f5dafc12deea8
developedbyjk <71823598+developedbyjk@users.noreply.github.com>
2024-01-13 16:49:03 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div class="container">
+        <p>- we need to have mistakes in life inorder to learn - <i>13 jan</i></p>
+    </div>
+    </>
+  )
+}
+
+export default App
