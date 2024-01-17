853e501b17230cc8e6b45715a11e521391ee1517
developedbyjk <71823598+developedbyjk@users.noreply.github.com>
2024-01-17 22:13:20 +0530

    17 jan

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,12 @@ function App() {
   return (
     <>
     <div class="container">
-        <p>- we need to have mistakes in life inorder to learn - <i>13 jan</i></p>
+      #Learnings✨
+        <p>- we need to have mistakes in life inorder to learn 🎉- <i>13 jan</i></p>
         <br />
-        <p>- Never Judge Too Quicky & Never say something when you are angry - <i>14 jan</i></p>
-        <p>- Life is limited to have fun and enjoy - <i>15 jan</i></p>
+        <p>- Never Judge Too Quicky & Never say something when you are angry 😅 - <i>14 jan</i></p>
+        <p>- Life is limited to have fun and enjoy 😉 - <i>15 jan</i></p>
+        <p>- Playing with pets makes you happpy 😄 - <i>15 jan</i></p>
     </div>
     </>
   )
