1533f6550360249cf8b902b60959321a4249a1e0
developedbyjk <71823598+developedbyjk@users.noreply.github.com>
2024-01-18 11:36:13 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
         <p>- we need to have mistakes in life inorder to learn 🎉- <i>13 jan</i></p>
         <p>- Never Judge Too Quicky & Never say something when you are angry 😅 - <i>14 jan</i></p>
         <p>- Life is limited to have fun and enjoy 😉 - <i>15 jan</i></p>
-        <p>- Playing with pets makes you happpy 😄 - <i>15 jan</i></p>
+        <p>- Playing with pets makes you happpy 😄 - <i>17 jan</i></p>
     </div>
     </>
   )
